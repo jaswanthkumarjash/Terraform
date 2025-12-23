2d92af0d55ebb6fdf5c994cdaa2ce3f4d610762e
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-12-23 16:04:23 +0530

    Terraform Practice

--- data-source/ami-id.tf
+++ data-source/ami-id.tf
@@ -1,21 +1,32 @@
-data "aws_ami" "example" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "^myami-[0-9]{3}"
-  owners           = ["self"]
 
-  filter {
-    name   = "name"
-    values = ["myami-*"]
-  }
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "ami_id" {
+    value = data.aws_ami.joindevops.id
+}
+
+data "aws_instance" "mongodb" {
+    instance_id = "i-0355f99cd46d41298"
+}
+
+output "mongodb_info" {
+    value = data.aws_instance.mongodb.public_ip
 }
